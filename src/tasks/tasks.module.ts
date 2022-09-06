96404b700430976bbc2b60613b3199677693123b
farrel.edwinconsultants <farrel.edwinconsultants@gmail.com>
2022-09-06 09:00:29 +0700

    Update JWT auth

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,20 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksRepository } from './tasks.repository';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
-import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: 'topSecret51',
-      signOptions: {
-        expiresIn: 3600,
-      },
-    }),
-    TypeOrmModule.forFeature([TasksRepository])
-  ],
+  imports: [TypeOrmModule.forFeature([TasksRepository])],
   controllers: [TasksController],
   providers: [TasksService],
 })
